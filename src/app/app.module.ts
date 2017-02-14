3e83ad0b412a810d3c47f6a7337d3454921632d5
steven <steven.mollie@hotmail.com>
2017-02-14 22:03:03 +0100

    about and contact components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
