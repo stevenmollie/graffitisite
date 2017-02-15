807cbb1d45712672fb92e68efef97f148b115184
steven <steven.mollie@hotmail.com>
2017-02-15 22:06:41 +0100

    nav initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,14 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      {path:"about", component: AboutComponent},
+      {path:"contact", component: ContactComponent},
+      {path:"home", component: AboutComponent},
+      {path:"**", redirectTo: "home"},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
